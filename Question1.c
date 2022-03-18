38f44fd16dce3c0cff608b1c5687c11c9736d436
Reprodux <ha.briann@gmail.com>
2022-03-18 18:19:14 -0400

    Update Question1.c

---
+++ Question1.c
@@ -0,0 +1,13 @@
+/*
+Author:  Brian Ha - 190376250 - haxx6250@mylaurier.ca - Github: https://github.com/Reprodux 
+         Eric Wildfong, 190559940 - wild9940@mylaurier.ca - Github: https://github.com/elw-arduino
+Repository:
+         https://github.com/elw-arduino/CP386-A4
+
+ -------------------------------------
+ File:    Question1.c
+ Description: Example of Banker's Algorithm
+ Version  3/18/2022
+ -------------------------------------
+ */
+
