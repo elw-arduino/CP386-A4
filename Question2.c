78145d9b0ea2c49574cf577eda25c4144c21f5db
Reprodux <ha.briann@gmail.com>
2022-03-26 18:48:38 -0400

    Create Question2.c

---
+++ Question2.c
@@ -0,0 +1,12 @@
+/*
+Author:  Brian Ha - 190376250 - haxx6250@mylaurier.ca - Github: https://github.com/Reprodux 
+         Eric Wildfong, 190559940 - wild9940@mylaurier.ca - Github: https://github.com/elw-arduino
+Repository:
+         https://github.com/elw-arduino/CP386-A4
+
+ -------------------------------------
+ File:    Question2.c
+ Description: Example of Best-Fit Algo
+ Version  3/24/2022
+ -------------------------------------
+ */
